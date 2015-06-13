7a8b886c1340f668f0dd2453250f39a17f648be3
eric <eric.bo.zhang@gmail.com>
2015-06-13 00:33:11 -0700

    Add Channel Function

--- nodeChat/app.js
+++ nodeChat/app.js
@@ -16,12 +16,43 @@ var storeMessage = function(name, data) {
     
 }
 
+var storeUsers = function(name) {
+    var username = JSON.stringify({name: name});
+    redisClient.lpush("users", username, function() {
+        redisClient.ltrim("users", 0, 9);
+    });
+}
+
+var storeChannel = function(name, callback) {
+    var dup = false;
+    redisClient.lrange("channel", 0, -1, function(err, channelNames) {
+        for (var i = 0; i < channelNames.length; i++) {
+            var nameJson = JSON.parse(channelNames[i]);
+            if (nameJson.name == name) {
+                dup = true;
+                break;
+            }
+        }
+        if (!dup) {
+            var channelName = JSON.stringify({name: name});
+            redisClient.lpush("channel", channelName);
+            redisClient.lrange("channel", 0, -1, function (err, newChannels) {
+                callback(null, newChannels);
+            });
+        } else {
+            callback(null, channelNames);
+        }
+
+    });
+}
+
 io.on('connection', function(client){
   var chanel = "";
   client.on('join', function(name) {
     client.nickname = name;
   });
-  client.on('chanel', function(data) {
+
+  client.on('channel', function(data) {
     chanel = data;
     client.join(data);
       
@@ -30,10 +61,34 @@ io.on('connection', function(client){
     messages.forEach(function(message) {
         message = JSON.parse(message);
         client.emit("chat message", message.name + ": " + message.data);
-        
     });
     });
-      
+
+    storeChannel(data, function(err, channelNames) {
+        if (!err) {
+            channelNames = channelNames.reverse();
+            channelNames.forEach(function(channel) {
+                channel = JSON.parse(channel);
+                client.emit("new channel", channel.name);
+            });
+        }
+    });
+
+
+  });
+
+  client.on('new user', function(name) {
+      client.broadcast.emit("new user", name);
+      redisClient.lrange("users", 0, -1, function(err, onlineUsers) {
+          onlineUsers = onlineUsers.reverse();
+          onlineUsers.forEach(function(user) {
+              user = JSON.parse(user);
+              client.emit("new user", user.name);
+
+          });
+          client.emit("new user", name);
+      });
+      storeUsers(name);
   });
     
   client.on('chat message', function(message){
@@ -42,6 +97,17 @@ io.on('connection', function(client){
     client.emit("chat message", nickname +  ": " + message);
     storeMessage(nickname, message);
   });
+
+  client.on('join group', function(groupname) {
+      console.log("=====server===join group=======" + data);
+      client.broadcast.emit('join group', data);
+      client.join(groupname);
+  });
+  client.on('new group', function(data) {
+      console.log("========server=======" + data);
+      client.broadcast.emit('join group', data);
+  });
+
 });
 
 http.listen(3000, function(){
